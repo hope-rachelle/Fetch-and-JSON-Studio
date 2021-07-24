487cab4ef8e9494f6e705087344e546455803d0c
Hope Ciuffa <hopeciuffa@gmail.com>
2021-07-24 17:26:07 -0500

    added code
    had to use axios to make it work

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    
+    axios.get("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        //console.log(response);
+        
+    let astronauts = document.getElementById("container");
+    for(i=0; i<response.data.length; i++){
+            astronauts.innerHTML=`
+    <div class="astronaut">
+        <div class="bio">
+      <h3>${response.data[i].firstName} ${response.data[i].lastName}</h3>
+      <ul>
+         <li>Hours in Space: ${response.data[i].hoursInSpace}</li>
+         <li>Active: ${response.data[i].active}</li>
+         <li>Skills: ${response.data[i].skills}</li>
+      </ul>
+         </div>
+   <img class="avatar" src=${response.data[i].picture}>
+</div>`}
+                
+    
+    })
+})
